7c8217bf16ace799e1ba48d13cdc44132ba1ef61
Welchd1 <dtw.welch@gmail.com>
2015-02-26 23:48:15 -0500

    initial commit

---
+++ src/java/edu/clemson/resolve/plugin/RESOLVELanguage.java
@@ -0,0 +1,11 @@
+package java.edu.clemson.resolve.plugin;
+
+import com.intellij.lang.Language;
+
+public class RESOLVELanguage extends Language {
+    public static final RESOLVELanguage INSTANCE = new RESOLVELanguage();
+
+    private RESOLVELanguage() {
+        super("RESOLVE");
+    }
+}
