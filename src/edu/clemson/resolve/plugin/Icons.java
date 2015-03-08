ba198f0db346302cf675179e26b7edeb5f49639a
Welchd1 <dtw.welch@gmail.com>
2015-03-08 14:10:16 -0400

    committed resources and the icons class

---
+++ src/edu/clemson/resolve/plugin/Icons.java
@@ -0,0 +1,10 @@
+package edu.clemson.resolve.plugin;
+
+import com.intellij.openapi.util.IconLoader;
+
+import javax.swing.*;
+
+public class Icons {
+
+    public static final Icon FILE = IconLoader.getIcon("/edu/clemson/resolve/icons/resolve.png");
+}
