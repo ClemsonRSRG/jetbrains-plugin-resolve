0f509f7a3108d3bbb11d6e2b9583ae957b781508
Welchd1 <dtw.welch@gmail.com>
2015-03-08 16:51:19 -0400

    going back to follow the tutorial first -- then later we'll worry about how to use the antlr tree..

---
+++ src/java/edu/clemson/resolve/plugin/RESOLVEPluginController.java
@@ -0,0 +1,47 @@
+package edu.clemson.resolve.plugin;
+
+import com.intellij.openapi.components.ProjectComponent;
+import com.intellij.openapi.diagnostic.Logger;
+import com.intellij.openapi.project.Project;
+import org.jetbrains.annotations.NotNull;
+
+public class RESOLVEPluginController implements ProjectComponent {
+
+    public static final Logger LOG =
+            Logger.getInstance("RESOLVE RESOLVEPluginController");
+    public Project project;
+
+    public RESOLVEPluginController(Project project) {
+        this.project = project;
+    }
+
+    public static RESOLVEPluginController getInstance(Project project) {
+        return project.getComponent(RESOLVEPluginController.class);
+    }
+
+    @Override
+    public void projectOpened() {
+
+    }
+
+    @Override
+    public void projectClosed() {
+
+    }
+
+    @Override
+    public void initComponent() {
+
+    }
+
+    @Override
+    public void disposeComponent() {
+
+    }
+
+    @NotNull
+    @Override
+    public String getComponentName() {
+        return "resolve.ProjectComponent";
+    }
+}
---
+++ src/java/edu/clemson/resolve/plugin/psi/RESOLVETokenType.java
@@ -0,0 +1,17 @@
+package edu.clemson.resolve.plugin.psi;
+
+import com.intellij.psi.tree.IElementType;
+import edu.clemson.resolve.plugin.RESOLVELanguage;
+import org.jetbrains.annotations.NonNls;
+import org.jetbrains.annotations.NotNull;
+
+public class RESOLVETokenType extends IElementType {
+    public RESOLVETokenType(@NotNull @NonNls String debugName) {
+        super(debugName, RESOLVELanguage.INSTANCE);
+    }
+
+    @Override
+    public String toString() {
+        return "SimpleTokenType." + super.toString();
+    }
+}
