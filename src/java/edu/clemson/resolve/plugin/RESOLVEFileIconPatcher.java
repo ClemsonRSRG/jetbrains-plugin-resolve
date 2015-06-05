b16c5be64ef1fb7365c7338a50e40bc7e86422de
Welchd1 <dtw.welch@gmail.com>
2015-06-05 18:08:32 -0400

    added some documentation

--- src/java/edu/clemson/resolve/plugin/RESOLVEFileIconPatcher.java
+++ src/java/edu/clemson/resolve/plugin/RESOLVEFileIconPatcher.java
@@ -11,6 +11,10 @@ import edu.clemson.resolve.plugin.psi.impl.RPrecisModuleImpl;
 
 import javax.swing.*;
 
+/**
+ * Dynamically updates (patches) icons for {@link PsiFile} instances based on
+ * module declared within.
+ */
 public class RESOLVEFileIconPatcher implements FileIconPatcher {
 
     @Override public Icon patchIcon(Icon baseIcon, VirtualFile file,
@@ -24,12 +28,11 @@ public class RESOLVEFileIconPatcher implements FileIconPatcher {
     @SuppressWarnings("unchecked") private static Icon replaceIcon(
             VirtualFile file, int flags, Project project, Icon baseIcon) {
         final PsiFile f = PsiManager.getInstance(project).findFile(file);
-        //return PsiTreeUtil.getChildOfType(file, ParserClass.class);
 
         //Todo: this might be prohibitively expensive (as findChildOfAnyType
         //is doing a depth first search). Though in the future hopefully our
-        //psi structure will be better defined so we can just make the following
-        //call: PsiTreeUtil.getChildOfType(f, ParserClass.class);
+        //psi structure will be better defined so we can just do the following:
+        // PsiTreeUtil.getChildOfType(f, ParserClass.class);
         if (PsiTreeUtil.findChildOfAnyType(f, RPrecisModuleImpl.class) != null) {
             return RESOLVEIcons.PRECIS;
         }
