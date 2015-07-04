fc19b5cb871b3e6bf48fd6c089179fd778b6e7e4
Welchd1 <dtw.welch@gmail.com>
2015-07-04 01:40:21 -0400

    top lvl pattern

--- src/java/edu/clemson/resolve/plugin/completion/RESOLVEKeywordCompletionContributor.java
+++ src/java/edu/clemson/resolve/plugin/completion/RESOLVEKeywordCompletionContributor.java
@@ -2,6 +2,20 @@ package edu.clemson.resolve.plugin.completion;
 
 import com.intellij.codeInsight.completion.CompletionContributor;
 import com.intellij.codeInsight.completion.CompletionType;
+import com.intellij.patterns.CollectionPattern;
+import com.intellij.patterns.ElementPattern;
+import com.intellij.patterns.PsiElementPattern;
+import com.intellij.patterns.PsiFilePattern;
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiErrorElement;
+import edu.clemson.resolve.plugin.RESOLVETokenTypes;
+import edu.clemson.resolve.plugin.psi.RFile;
+
+import java.util.Collection;
+
+import static com.intellij.patterns.PlatformPatterns.psiElement;
+import static com.intellij.patterns.PlatformPatterns.psiFile;
+import static com.intellij.patterns.StandardPatterns.*;
 
 public class RESOLVEKeywordCompletionContributor extends CompletionContributor {
 
@@ -10,4 +24,24 @@ public class RESOLVEKeywordCompletionContributor extends CompletionContributor {
                 new GoKeywordCompletionProvider(GoCompletionUtil.KEYWORD_PRIORITY, "for", "const", "var", "return", "if", "switch", "go",
                         "defer", "goto"));
     }
+
+    private static PsiElementPattern.Capture<PsiElement> topLevelPattern() {
+        return psiElement(RESOLVETokenTypes.)
+                .withParent(psiElement(PsiErrorElement.class).withParent(
+                        resolveFile()).isFirstAcceptedChild(psiElement()));
+    }
+
+    private static PsiFilePattern.Capture<RFile> resolveFile() {
+        CollectionPattern<PsiElement> collection = collection(PsiElement.class);
+        ElementPattern<Collection<PsiElement>> empty = collection.empty();
+        return psiFile(RFile.class).withChildren(collection.filter(
+                not(psiElement().whitespaceCommentEmptyOrError()), empty));
+    }
+
+
+
+
+    //then global module pattern, etc.
+
+    //operation procedure body pattern
 }
