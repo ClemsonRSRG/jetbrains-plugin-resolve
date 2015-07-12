5bcae02da0f54335d88323dd484b16a9f2cc9bd4
Welchd1 <dtw.welch@gmail.com>
2015-07-11 22:57:31 -0700

    runnable files work

---
+++ src/java/edu/clemson/resolve/plugin/runconfig/RESOLVERunConfigurationBase.java
@@ -0,0 +1,19 @@
+package edu.clemson.resolve.plugin.runconfig;
+
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * Created by daniel on 7/11/15.
+ */
+public abstract class RESOLVERunConfigurationBase<RunningState extends GoRunningState> {
+    @NotNull private String myWorkingDirectory = "";
+
+    @NotNull
+    public String getWorkingDirectory() {
+        return myWorkingDirectory;
+    }
+
+    public void setWorkingDirectory(@NotNull String workingDirectory) {
+        myWorkingDirectory = workingDirectory;
+    }
+}
