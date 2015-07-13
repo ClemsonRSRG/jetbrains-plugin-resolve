1c4e583093dbe406446ff2ba833a6ddbd974bea4
Welchd1 <dtw.welch@gmail.com>
2015-07-13 00:57:44 -0400

    added run config icon with shift

--- src/java/edu/clemson/resolve/plugin/RESOLVEIcons.java
+++ src/java/edu/clemson/resolve/plugin/RESOLVEIcons.java
@@ -2,11 +2,13 @@ package edu.clemson.resolve.plugin;
 
 import com.intellij.icons.AllIcons;
 import com.intellij.openapi.util.IconLoader;
+import com.intellij.ui.LayeredIcon;
 import com.sun.tools.corba.se.idl.toJavaPortable.Helper;
+import org.jetbrains.annotations.NotNull;
 
 import javax.swing.*;
 
-public class RESOLVEIcons {
+public interface RESOLVEIcons {
 
     public static final Icon FILE = IconLoader.getIcon("/edu/clemson/resolve/icons/file.png");
     public static final Icon CONCEPT = IconLoader.getIcon("/edu/clemson/resolve/icons/concept.png");
@@ -15,6 +17,17 @@ public class RESOLVEIcons {
     public static final Icon PRECIS = IconLoader.getIcon("/edu/clemson/resolve/icons/precis.png");
     public static final Icon IMPL = IconLoader.getIcon("/edu/clemson/resolve/icons/implementation.png");
     public static final Icon MODULE = IconLoader.getIcon("/edu/clemson/resolve/icons/module.png");
-    public static final Icon APPLICATION_RUN = AllIcons.Nodes.RunnableMark;
+    public static final Icon APPLICATION_RUN = Helper.createIconWithShift(MODULE, AllIcons.Nodes.RunnableMark);
 
+    class Helper {
+        @NotNull public static LayeredIcon createIconWithShift(
+                @NotNull final Icon base, Icon mark) {
+            LayeredIcon icon = new LayeredIcon(2) {
+                @Override public int getIconHeight() { return base.getIconHeight(); }
+            };
+            icon.setIcon(base, 0);
+            icon.setIcon(mark, 1, 0, base.getIconWidth() / 2);
+            return icon;
+        }
+    }
 }
