c8088bcf1a9b1b38822ca63d71527481169c7dcd
Welchd1 <dtw.welch@gmail.com>
2015-10-18 09:57:36 -0400

    to test lexer, will need to figure out how to just get the tokenstream from psiviewer

---
+++ tests/edu/clemson/resolve/plugin/lexer/RESOLVELexerTest.java
@@ -0,0 +1,33 @@
+package edu.clemson.resolve.plugin.lexer;
+
+import com.intellij.lexer.Lexer;
+import com.intellij.openapi.util.io.FileUtil;
+import com.intellij.openapi.util.text.StringUtil;
+import com.intellij.testFramework.LexerTestCase;
+
+import java.io.File;
+import java.io.IOException;
+
+public class RESOLVELexerTest extends LexerTestCase {
+
+    public void testHelloWorld() { doTest(); }
+
+    private void doTest() {
+        try {
+            String text = FileUtil.loadFile(new File("./testData/lexer/"
+                    + getTestName(false) + ".resolve"));
+            String actual = printTokens(StringUtil
+                    .convertLineSeparators(text.trim()), 0);
+            assertSameLinesWithFile(new File("testData/lexer/"
+                    + getTestName(false) + ".txt").getAbsolutePath(), actual);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override protected Lexer createLexer() { return new ResolveLexer(); }
+
+    @Override protected String getDirPath() {
+        return "../testData/lexer";
+    }
+}
