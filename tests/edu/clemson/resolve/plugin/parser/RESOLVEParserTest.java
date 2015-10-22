358214164bec4aae7e02f2bf74e2ca3ee4d1d90b
Welchd1 <dtw.welch@gmail.com>
2015-10-22 12:06:04 -0400

    started some basic parser tests

---
+++ tests/edu/clemson/resolve/plugin/parser/RESOLVEParserTest.java
@@ -0,0 +1,39 @@
+package edu.clemson.resolve.plugin.parser;
+
+import com.intellij.testFramework.ParsingTestCase;
+import edu.clemson.resolve.plugin.RESOLVEParserDefinition;
+
+public class RESOLVEParserTest extends ParsingTestCase {
+
+    public RESOLVEParserTest() {
+        super("parser", "resolve", new RESOLVEParserDefinition());
+    }
+
+    @Override protected String getTestDataPath() {
+        return "testData";
+    }
+
+    @Override protected boolean skipSpaces() {
+        return true;
+    }
+
+    protected void doTest(boolean checkErrors) {
+        super.doTest(true);
+        if (checkErrors) {
+            assertFalse(
+                    "PsiFile contains error elements",
+                    toParseTreeText(myFile, skipSpaces(), includeRanges())
+                            .contains("PsiErrorElement")
+            );
+        }
+    }
+
+    @Override protected void setUp() throws Exception {
+        super.setUp();
+        //TODO: register any matching extensions you might provide in the
+        //future here -- for instance, codeblock insertionhandler
+    }
+
+    public void testEmpty_File() { doTest(true); }
+    public void testEmpty_Module() { doTest(true); }
+}
