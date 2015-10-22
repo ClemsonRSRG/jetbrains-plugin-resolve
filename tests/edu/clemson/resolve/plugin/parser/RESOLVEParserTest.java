1d15b4a7e39158481ad5b63789c8a2e5802737f5
Welchd1 <dtw.welch@gmail.com>
2015-10-22 13:52:38 -0400

    added another recovery test

--- tests/edu/clemson/resolve/plugin/parser/RESOLVEParserTest.java
+++ tests/edu/clemson/resolve/plugin/parser/RESOLVEParserTest.java
@@ -39,6 +39,7 @@ public class RESOLVEParserTest extends ParsingTestCase {
     public void testEmpty_Module() { doTest(true); }
     public void testModule_Block_Error_Recover() { doTest(false); }
     public void testModule_Block_Error_Recover2() { doTest(false); }
+    public void testModule_Block_Error_Recover3() { doTest(false); }
     public void testOp_Error_Recover() { doTest(false); }
 
 }
