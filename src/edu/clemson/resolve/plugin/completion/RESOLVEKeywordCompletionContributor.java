32a798433556f3ccc353ffaad4721bcfc0b2d4a2
Welchd1 <dtw.welch@gmail.com>
2015-10-22 21:03:47 -0700

    started keyword completor

---
+++ src/edu/clemson/resolve/plugin/completion/RESOLVEKeywordCompletionContributor.java
@@ -0,0 +1,13 @@
+package edu.clemson.resolve.plugin.completion;
+
+import com.intellij.codeInsight.completion.CompletionContributor;
+import com.intellij.openapi.project.DumbAware;
+
+public class RESOLVEKeywordCompletionContributor
+        extends
+            CompletionContributor implements DumbAware {
+
+    public RESOLVEKeywordCompletionContributor() {
+
+    }
+}
