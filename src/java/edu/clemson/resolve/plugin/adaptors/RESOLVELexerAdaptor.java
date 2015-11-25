76fb0c0254c2508390191c75da67ef9a67771f0f
Welchd1 <dtw.welch@gmail.com>
2015-11-25 14:10:15 -0800

    lots of progress here, going to merge back to master

--- src/java/edu/clemson/resolve/plugin/adaptors/RESOLVELexerAdaptor.java
+++ src/java/edu/clemson/resolve/plugin/adaptors/RESOLVELexerAdaptor.java
@@ -9,23 +9,21 @@ import org.antlr.v4.runtime.Lexer;
 /** Adapts RESOLVE/Antlr specific needs to IntelliJ's. */
 public class RESOLVELexerAdaptor extends SimpleAntlrAdapter {
 
-    private static final AntlrLexerState INITIAL_STATE = new AntlrLexerState(Lexer.DEFAULT_MODE, null);
+    private static final AntlrLexerState INITIAL_STATE =
+            new AntlrLexerState(Lexer.DEFAULT_MODE, null);
 
     public RESOLVELexerAdaptor(Language language, Lexer lexer) {
         super(language, lexer);
     }
 
-    @Override
-    protected AntlrLexerState getInitialState() {
+    @Override protected AntlrLexerState getInitialState() {
         return INITIAL_STATE;
     }
 
-    @Override
-    protected AntlrLexerState getLexerState(Lexer lexer) {
+    @Override protected AntlrLexerState getLexerState(Lexer lexer) {
         if (lexer._modeStack.isEmpty()) {
             return new AntlrLexerState(lexer._mode, null);
         }
-
         return new AntlrLexerState(lexer._mode, lexer._modeStack);
     }
 }
