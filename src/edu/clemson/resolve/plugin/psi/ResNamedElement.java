571bd7b74219c5a38350ce2e64b1458dedc515e8
Welchd1 <dtw.welch@gmail.com>
2015-12-13 09:31:32 -0500

    doc++

--- src/edu/clemson/resolve/plugin/psi/ResNamedElement.java
+++ src/edu/clemson/resolve/plugin/psi/ResNamedElement.java
@@ -10,12 +10,25 @@ import org.jetbrains.annotations.Nullable;
 /**
  * Represents all named, declaration-like constructs in the language.
  *
- * NameElement FAQ:<p>
- * Q: How do I implement code completion?<br>
- * A: Define a completion.contributor extension of type {@link CompletionContributor}.
- * Or, if the place you want to complete in contains a {@link PsiReference}, just return the variants
- * you want to suggest from its {@link PsiReference#getVariants()} method as {@link String}s,
- * {@link com.intellij.psi.PsiElement}s, or better {@link LookupElement}s.<p>
+ * NamedElement FAQ:<p>
+ * Q: How do I know if my node should extend this?<br>
+ * A: If the element you're creating has a name and is <em>declared</em>
+ * (as opposed to referenced) either in a top level module or somewhere within
+ * a local operation {@link ResBlock}, then chances are you want to
+ * extend this.<p>
+ *
+ * Q: OK, but then why doesn't {@link ResMathDefinitionDecl} extend this?<br>
+ * A: That's a special case, made necessary by the ridiculous number of ways
+ * our language has for introducing mathematical operators to scope; for a more
+ * detailed explanation, refer to the doc in {@link ResMathDefinitionDecl}.<p>
+ *
+ * Q: I see that this class extends {@link ResTypeOwner}, but the node I want
+ * to extend {@code this} doesn't have a sensible {@link ResType}! Is this OK?<br>
+ * A: Sure, right now this is here mainly for convenience. Notice that the
+ * method is annotated {@code Nullable}, indicating that it's fine if your
+ * node has no real type info. In the future, this class might be adjusted so
+ * that it doesn't extend {@link ResTypeOwner} -- pushing that off to more
+ * specific extending classes (e.g.: {@link ResVarDef}, etc)<p>
  */
 public interface ResNamedElement
         extends
