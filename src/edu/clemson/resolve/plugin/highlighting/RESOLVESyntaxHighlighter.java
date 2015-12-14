5b27803fa4d7e48b1d7f2d7301637baeb60f4ac1
Welchd1 <dtw.welch@gmail.com>
2015-12-14 17:27:32 -0500

    add doc

--- src/edu/clemson/resolve/plugin/highlighting/RESOLVESyntaxHighlighter.java
+++ src/edu/clemson/resolve/plugin/highlighting/RESOLVESyntaxHighlighter.java
@@ -15,6 +15,28 @@ import java.util.Map;
 
 import static edu.clemson.resolve.plugin.highlighting.RESOLVESyntaxHighlightingColors.*;
 
+/**
+ * A highlighter is really just a mapping from token type to
+ * some text attributes using {@link #getTokenHighlights(IElementType)}.
+ * The reason that it returns an array, TextAttributesKey[], is
+ * that you might want to mix the attributes of a few known highlighters.
+ * A {@link TextAttributesKey} is just a name for that a theme
+ * or IDE skin can set. For example,
+ * {@link com.intellij.openapi.editor.DefaultLanguageHighlighterColors#KEYWORD}
+ * is the key that maps to what identifiers look like in the editor.
+ * To change it, see dialog: Editor > Colors & Fonts > Language Defaults.
+ *
+ * From <a href="http://www.jetbrains.org/intellij/sdk/docs/reference_guide/custom_language_support/syntax_highlighting_and_error_highlighting.html">doc</a>:
+ * "The mapping of the TextAttributesKey to specific attributes used
+ * in an editor is defined by the EditorColorsScheme class, and can
+ * be configured by the user if the plugin provides an appropriate
+ * configuration interface.
+ * ...
+ * The syntax highlighter returns the {@link TextAttributesKey}
+ * instances for each token type which needs special highlighting.
+ * For highlighting lexer errors, the standard TextAttributesKey
+ * for bad characters HighlighterColors.BAD_CHARACTER can be used."
+ */
 public class RESOLVESyntaxHighlighter extends SyntaxHighlighterBase {
 
     private static final Map<IElementType, TextAttributesKey> ATTRIBUTES =
