5eca114f41bb40940bc7ed9b10d5ed4932e0eb56
Welchd1 <dtw.welch@gmail.com>
2015-12-17 00:28:16 -0500

    freshen directories

--- src/edu/clemson/resolve/jetbrains/lexer/gen_lexer.sh
+++ src/edu/clemson/resolve/jetbrains/lexer/gen_lexer.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-~/Documents/intellij-community/tools/lexer/jflex-1.4/bin/jflex --skel ~/Documents/intellij-community/tools/lexer/idea-flex.skeleton --charat --nobak ResLexer.flex -d ../../../../../../gen/edu/clemson/resolve/plugin/lexer
+~/Documents/intellij-community/tools/lexer/jflex-1.4/bin/jflex --skel ~/Documents/intellij-community/tools/lexer/idea-flex.skeleton --charat --nobak ResLexer.flex -d ../../../../../../gen/edu/clemson/resolve/jetbrains/lexer
 
