e1aeaf266961ec73d696ec1b86e14c711cb66bf9
Welchd1 <dtw.welch@gmail.com>
2016-04-19 00:37:27 -0400

    added some prelim beginnings for modules

--- src/edu/clemson/resolve/jetbrains/RESOLVELanguage.java
+++ src/edu/clemson/resolve/jetbrains/RESOLVELanguage.java
@@ -1,11 +1,24 @@
 package edu.clemson.resolve.jetbrains;
 
 import com.intellij.lang.Language;
+import com.intellij.openapi.fileTypes.LanguageFileType;
+import org.jetbrains.annotations.Nullable;
 
 public class RESOLVELanguage extends Language {
     public static final RESOLVELanguage INSTANCE = new RESOLVELanguage();
 
     private RESOLVELanguage() {
         super("RESOLVE");
+    }
+
+    @Nullable
+    @Override
+    public LanguageFileType getAssociatedFileType() {
+        return RESOLVEFileType.INSTANCE;
+    }
+
+    @Override
+    public boolean isCaseSensitive() {
+        return true;
     }
 }
---
+++ src/edu/clemson/resolve/jetbrains/RESOLVEModuleType.java
@@ -0,0 +1,9 @@
+package edu.clemson.resolve.jetbrains;
+
+import sun.security.pkcs11.Secmod;
+
+/**
+ * Created by daniel on 4/19/16.
+ */
+public class RESOLVEModuleType {
+}
