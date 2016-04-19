85e7c245ad8d87a87d841e9248fb73dba25665e7
Welchd1 <dtw.welch@gmail.com>
2016-04-19 10:22:10 -0400

    updated icons for modules

---
+++ src/edu/clemson/resolve/jetbrains/RESOLVEConstants.java
@@ -0,0 +1,14 @@
+package edu.clemson.resolve.jetbrains;
+
+
+import org.jetbrains.annotations.NonNls;
+
+public class RESOLVEConstants {
+    public static final String MODULE_TYPE_ID = "RESOLVE_MODULE";
+    public static final String SDK_TYPE_ID = "RESOLVE SDK";
+    public static final String RESOLVE_PATH = "RESOLVEPATH";
+
+    @NonNls public static final String RESOLVE = "RESOLVE";
+
+    private RESOLVEConstants() {}
+}
--- src/edu/clemson/resolve/jetbrains/RESOLVEModuleType.java
+++ src/edu/clemson/resolve/jetbrains/RESOLVEModuleType.java
@@ -1,9 +1,62 @@
 package edu.clemson.resolve.jetbrains;
 
+import com.intellij.ide.util.projectWizard.ModuleWizardStep;
+import com.intellij.ide.util.projectWizard.ProjectJdkForModuleStep;
+import com.intellij.ide.util.projectWizard.WizardContext;
+import com.intellij.openapi.module.ModuleType;
+import com.intellij.openapi.module.ModuleTypeManager;
+import com.intellij.openapi.roots.ui.configuration.ModulesProvider;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 import sun.security.pkcs11.Secmod;
 
-/**
- * Created by daniel on 4/19/16.
- */
-public class RESOLVEModuleType {
+import javax.swing.*;
+
+public class RESOLVEModuleType extends ModuleType<RESOLVEModuleBuilder> {
+
+    public RESOLVEModuleType() {
+        super(RESOLVEConstants.MODULE_TYPE_ID);
+    }
+
+    @NotNull public static RESOLVEModuleType getInstance() {
+        return (RESOLVEModuleType) ModuleTypeManager.getInstance()
+                .findByID(RESOLVEConstants.MODULE_TYPE_ID);
+    }
+
+    @NotNull @Override public RESOLVEModuleBuilder createModuleBuilder() {
+        return new RESOLVEModuleBuilder();
+    }
+
+    @NotNull @Override public String getName() {
+        return "RESOLVE Module";
+    }
+
+    @NotNull @Override public String getDescription() {
+        return "RESOLVE modules are used for developing <b>RESOLVE</b> " +
+                "component based software.";
+    }
+
+    @Nullable @Override public Icon getBigIcon() {
+        return RESOLVEIcons.MODULE;
+    }
+
+    @Nullable
+    @Override
+    public Icon getNodeIcon(boolean isOpened) {
+        return RESOLVEIcons.TOOL_ICON;
+    }
+
+    @NotNull
+    @Override
+    public ModuleWizardStep[] createWizardSteps(@NotNull WizardContext wizardContext,
+                                                @NotNull final GoModuleBuilder moduleBuilder,
+                                                @NotNull ModulesProvider modulesProvider) {
+        return new ModuleWizardStep[]{new ProjectJdkForModuleStep(wizardContext, GoSdkType.getInstance()) {
+            @Override
+            public void updateDataModel() {
+                super.updateDataModel();
+                moduleBuilder.setModuleJdk(getJdk());
+            }
+        }};
+    }
 }
