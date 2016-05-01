770fa2f962a4d3b224d1dc414be687ff38edcaf8
Welchd1 <dtw.welch@gmail.com>
2016-04-30 22:52:04 -0400

    added psi

---
+++ gen/edu/clemson/resolve/jetbrains/psi/ResMathAngleOutfixApplyExp.java
@@ -0,0 +1,19 @@
+// This is a generated file. Not intended for manual editing.
+package edu.clemson.resolve.jetbrains.psi;
+
+import java.util.List;
+import org.jetbrains.annotations.*;
+import com.intellij.psi.PsiElement;
+
+public interface ResMathAngleOutfixApplyExp extends ResMathExp {
+
+  @Nullable
+  ResMathExp getMathExp();
+
+  @Nullable
+  PsiElement getGreater();
+
+  @NotNull
+  PsiElement getLess();
+
+}
---
+++ gen/edu/clemson/resolve/jetbrains/psi/ResMathPrefixGeneralizedSqbrApplyExp.java
@@ -0,0 +1,19 @@
+// This is a generated file. Not intended for manual editing.
+package edu.clemson.resolve.jetbrains.psi;
+
+import java.util.List;
+import org.jetbrains.annotations.*;
+import com.intellij.psi.PsiElement;
+
+public interface ResMathPrefixGeneralizedSqbrApplyExp extends ResMathExp {
+
+  @NotNull
+  List<ResMathExp> getMathExpList();
+
+  @NotNull
+  PsiElement getLbrack();
+
+  @Nullable
+  PsiElement getRbrack();
+
+}
