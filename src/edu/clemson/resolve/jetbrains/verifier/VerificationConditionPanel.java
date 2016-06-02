3177f482c24a5a453ec1bbf23ee48d25d5f87acb
Welchd1 <dtw.welch@gmail.com>
2016-06-02 02:08:50 -0400

    more work on ui

--- src/edu/clemson/resolve/jetbrains/verifier/VerificationConditionPanel.java
+++ src/edu/clemson/resolve/jetbrains/verifier/VerificationConditionPanel.java
@@ -6,10 +6,19 @@ import javax.swing.*;
  * Created by daniel on 6/1/16.
  */
 public class VerificationConditionPanel {
-    private JPanel panel1;
+    private JPanel outerMostPanel;
     private JLabel vcNumberLabel;
     private JPanel titlePanel;
     private JLabel vcDescriptionLabel;
     private JTextArea goalText;
-    private JTextArea textArea1;
+    private JTextArea givensText;
+    public VerifierPanel verifierPanel;
+
+    public VerificationConditionPanel(final VerifierPanel verifierPanel) {
+        this.verifierPanel = verifierPanel;
+    }
+
+    public JPanel getComponent() {
+        return outerMostPanel;
+    }
 }
--- src/edu/clemson/resolve/jetbrains/verifier/VerifierPanel.java
+++ src/edu/clemson/resolve/jetbrains/verifier/VerifierPanel.java
@@ -2,6 +2,7 @@ package edu.clemson.resolve.jetbrains.verifier;
 
 import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.project.Project;
+import com.intellij.openapi.ui.Splitter;
 
 import java.awt.*;
 import java.util.ArrayList;
@@ -13,11 +14,27 @@ import javax.swing.*;
 public class VerifierPanel extends JPanel {
 
     public static final Logger LOG = Logger.getInstance("RESOLVE VerifierPanel");
+    private VerificationConditionPanel vcPanel;
+    private final Project project;
 
     public VerifierPanel(Project project) {
-        this.setCursor(new Cursor(Cursor.HAND_CURSOR));
+        //this.setCursor(new Cursor(Cursor.HAND_CURSOR));
+        this.project = project;
         setPreferredSize(new Dimension(getPreferredSize().width, getPreferredSize().height));
         revalidate();
+        createGUI();
     }
 
+    private void createGUI() {
+        this.setLayout(new BorderLayout());
+
+        Splitter splitPane = new Splitter();
+        this.vcPanel = getVerificationConditionPanel();
+        splitPane.setFirstComponent(vcPanel.getComponent());
+    }
+
+    public VerificationConditionPanel getVerificationConditionPanel() {
+        LOG.info("createVerificationConditionPanel" + " " + project.getName());
+        return new VerificationConditionPanel(this);
+    }
 }
