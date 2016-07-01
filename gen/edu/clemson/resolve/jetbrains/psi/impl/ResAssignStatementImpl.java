9de2a6c98516e1fbda21c137eaae76217ad0a958
Welchd1 <dtw.welch@gmail.com>
2016-07-01 00:06:01 -0400

    commit psi and lexer work

---
+++ gen/edu/clemson/resolve/jetbrains/psi/impl/ResAssignStatementImpl.java
@@ -0,0 +1,40 @@
+// This is a generated file. Not intended for manual editing.
+package edu.clemson.resolve.jetbrains.psi.impl;
+
+import java.util.List;
+import org.jetbrains.annotations.*;
+import com.intellij.lang.ASTNode;
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiElementVisitor;
+import com.intellij.psi.util.PsiTreeUtil;
+import static edu.clemson.resolve.jetbrains.ResTypes.*;
+import edu.clemson.resolve.jetbrains.psi.*;
+
+public class ResAssignStatementImpl extends ResStatementImpl implements ResAssignStatement {
+
+  public ResAssignStatementImpl(ASTNode node) {
+    super(node);
+  }
+
+  public void accept(@NotNull ResVisitor visitor) {
+    visitor.visitAssignStatement(this);
+  }
+
+  public void accept(@NotNull PsiElementVisitor visitor) {
+    if (visitor instanceof ResVisitor) accept((ResVisitor)visitor);
+    else super.accept(visitor);
+  }
+
+  @Override
+  @NotNull
+  public List<ResExp> getExpList() {
+    return PsiTreeUtil.getChildrenOfTypeAsList(this, ResExp.class);
+  }
+
+  @Override
+  @NotNull
+  public PsiElement getColonEquals() {
+    return findNotNullChildByType(COLON_EQUALS);
+  }
+
+}
---
+++ gen/edu/clemson/resolve/jetbrains/psi/impl/ResMathAlternativeExpImpl.java
@@ -0,0 +1,46 @@
+// This is a generated file. Not intended for manual editing.
+package edu.clemson.resolve.jetbrains.psi.impl;
+
+import java.util.List;
+import org.jetbrains.annotations.*;
+import com.intellij.lang.ASTNode;
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiElementVisitor;
+import com.intellij.psi.util.PsiTreeUtil;
+import static edu.clemson.resolve.jetbrains.ResTypes.*;
+import edu.clemson.resolve.jetbrains.psi.*;
+
+public class ResMathAlternativeExpImpl extends ResMathExpImpl implements ResMathAlternativeExp {
+
+  public ResMathAlternativeExpImpl(ASTNode node) {
+    super(node);
+  }
+
+  public void accept(@NotNull ResVisitor visitor) {
+    visitor.visitMathAlternativeExp(this);
+  }
+
+  public void accept(@NotNull PsiElementVisitor visitor) {
+    if (visitor instanceof ResVisitor) accept((ResVisitor)visitor);
+    else super.accept(visitor);
+  }
+
+  @Override
+  @NotNull
+  public ResMathAlternativesList getMathAlternativesList() {
+    return findNotNullChildByClass(ResMathAlternativesList.class);
+  }
+
+  @Override
+  @NotNull
+  public PsiElement getDblLbrace() {
+    return findNotNullChildByType(DBL_LBRACE);
+  }
+
+  @Override
+  @NotNull
+  public PsiElement getDblRbrace() {
+    return findNotNullChildByType(DBL_RBRACE);
+  }
+
+}
