f2711e05ac28d4c91c96bad74a1ae72d86ca61ba
dtwelch <dtw.welch@gmail.com>
2016-10-03 21:55:07 -0400

    inserting at caret

--- src/edu/clemson/resolve/jetbrains/verifier/MathSymbolPanel.java
+++ src/edu/clemson/resolve/jetbrains/verifier/MathSymbolPanel.java
@@ -1,13 +1,19 @@
 package edu.clemson.resolve.jetbrains.verifier;
 
+import com.intellij.openapi.command.WriteCommandAction;
 import com.intellij.openapi.editor.Caret;
+import com.intellij.openapi.editor.Document;
 import com.intellij.openapi.editor.Editor;
+import com.intellij.openapi.editor.event.DocumentEvent;
+import com.intellij.openapi.editor.event.DocumentListener;
 import com.intellij.openapi.editor.ex.EditorEx;
 import com.intellij.openapi.editor.markup.HighlighterLayer;
 import com.intellij.openapi.editor.markup.HighlighterTargetArea;
+import com.intellij.openapi.editor.markup.RangeHighlighter;
 import com.intellij.openapi.editor.markup.TextAttributes;
 import com.intellij.openapi.fileEditor.FileEditorManager;
 import com.intellij.openapi.project.Project;
+import com.intellij.psi.PsiDocumentManager;
 import com.intellij.ui.JBColor;
 import com.intellij.ui.components.JBPanel;
 import com.intellij.ui.components.JBScrollPane;
@@ -25,6 +31,10 @@ import java.awt.*;
 import java.awt.event.FocusEvent;
 import java.awt.event.FocusListener;
 
+/**
+ * Displays a hierarchy of useful math symbols and the corresponding live template commands to insert them into
+ * the active editor.
+ */
 public class MathSymbolPanel extends JBPanel {
 
     private final JTree tree;
@@ -34,36 +44,65 @@ public class MathSymbolPanel extends JBPanel {
         createSections(top);
 
         this.tree = new Tree(top);
-        this.tree.addFocusListener(new FocusListener() {
+        /*this.tree.addFocusListener(new FocusListener() {
+            private RangeHighlighter activeHighlighter = null;
+
             @Override
             public void focusGained(FocusEvent e) {
-                Editor editor = FileEditorManager.getInstance(project).getSelectedTextEditor();
-                if (editor instanceof EditorEx) {
-                    //int x = ((EditorEx)editor).getExpectedCaretOffset();
-                    TextAttributes y = new TextAttributes();
-                    //HighlighterTargetArea.EXACT_RANGE
-                    y.setBackgroundColor(JBColor.GREEN);
 
-                    ((EditorEx) editor).getMarkupModel().addRangeHighlighter(((EditorEx) editor).getExpectedCaretOffset(),
-                            ((EditorEx) editor).getExpectedCaretOffset() + 1, HighlighterLayer.ELEMENT_UNDER_CARET, y,
+                if (editor instanceof EditorEx) {
+                    TextAttributes y = new TextAttributes();
+                    y.setBackgroundColor(JBColor.GREEN);
+                    int expectedOffset = ((EditorEx) editor).getCaretModel().getOffset();
+
+                    activeHighlighter = ((EditorEx) editor).getMarkupModel().addRangeHighlighter(
+                            expectedOffset-1, expectedOffset, HighlighterLayer.CARET_ROW, y,
                             HighlighterTargetArea.EXACT_RANGE);
+                    activeHighlighter.setGreedyToLeft(false);
+                    activeHighlighter.setGreedyToRight(true);
+
                 }
-                System.out.println("Focus Gained!!!!!!!");
             }
 
             @Override
             public void focusLost(FocusEvent e) {
-                System.out.println("Focus Lost!!!!!!!");
+                if (activeHighlighter != null && editor != null) {
+                    editor.getMarkupModel().removeAllHighlighters();
+                    activeHighlighter = null;
+                }
             }
-        });
+        });*/
+
         //fires when an element is selected
-     /*   tree.addTreeSelectionListener(new TreeSelectionListener() {
+        tree.addTreeSelectionListener(new TreeSelectionListener() {
             @Override
             public void valueChanged(TreeSelectionEvent e) {
-                setEditorCaretEnabledAndVisible(project);
+                Editor editor = FileEditorManager.getInstance(project).getSelectedTextEditor();
+                if (editor == null) return;
+                int tailOffset = editor.getCaretModel().getOffset();
+                Document document = editor.getDocument();
+                PsiDocumentManager.getInstance(project).commitDocument(document);
+                DefaultMutableTreeNode node = (DefaultMutableTreeNode) tree.getLastSelectedPathComponent();
+                if (node == null) return;
+                if (!(node.getUserObject() instanceof SymbolInfo)) return;
+
+               // e.getNewLeadSelectionPath().getLastPathComponent().
+                SymbolInfo s = (SymbolInfo)node.getUserObject();
+
+                Runnable runnable = new Runnable() {
+                    @Override
+                    public void run() {
+                        document.insertString(tailOffset, s.symbol);
+                    }
+                };
+                WriteCommandAction.runWriteCommandAction(project, runnable);
+
+                //document.insertString(tailOffset, s.symbol);
+                editor.getCaretModel().moveToOffset(tailOffset + 1);
+
             }
         });
-
+/*
         //fires when a section is opened or closed.
         tree.addTreeExpansionListener(new TreeExpansionListener() {
             @Override
