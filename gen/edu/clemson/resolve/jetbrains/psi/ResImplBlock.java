a974bac05d5c2ae13ec47bf9dcaa8f3a4420463f
Welchd1 <dtw.welch@gmail.com>
2016-11-16 12:47:25 -0500

    commit psi

---
+++ gen/edu/clemson/resolve/jetbrains/psi/ResImplBlock.java
@@ -0,0 +1,29 @@
+// This is a generated file. Not intended for manual editing.
+package edu.clemson.resolve.jetbrains.psi;
+
+import java.util.List;
+import org.jetbrains.annotations.*;
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.ResolveState;
+import com.intellij.psi.scope.PsiScopeProcessor;
+
+public interface ResImplBlock extends ResBlock {
+
+  @NotNull
+  List<ResFacilityDecl> getFacilityDeclList();
+
+  @NotNull
+  List<ResMathStandardDefnDecl> getMathStandardDefnDeclList();
+
+  @NotNull
+  List<ResOperationProcedureDecl> getOperationProcedureDeclList();
+
+  @NotNull
+  List<ResProcedureDecl> getProcedureDeclList();
+
+  @NotNull
+  List<ResTypeReprDecl> getTypeReprDeclList();
+
+  boolean processDeclarations(PsiScopeProcessor processor, ResolveState state, PsiElement lastParent, PsiElement place);
+
+}
---
+++ gen/edu/clemson/resolve/jetbrains/psi/ResPrecisExtensionModuleDecl.java
@@ -0,0 +1,31 @@
+// This is a generated file. Not intended for manual editing.
+package edu.clemson.resolve.jetbrains.psi;
+
+import java.util.List;
+import org.jetbrains.annotations.*;
+import com.intellij.psi.PsiElement;
+
+public interface ResPrecisExtensionModuleDecl extends ResModuleDecl {
+
+  @Nullable
+  ResPrecisBlock getPrecisBlock();
+
+  @NotNull
+  List<ResReferenceExp> getReferenceExpList();
+
+  @Nullable
+  ResUsesList getUsesList();
+
+  @Nullable
+  PsiElement getEnd();
+
+  @NotNull
+  PsiElement getExtension();
+
+  @Nullable
+  PsiElement getFor();
+
+  @Nullable
+  PsiElement getWith();
+
+}
