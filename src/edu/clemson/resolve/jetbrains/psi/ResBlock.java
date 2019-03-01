8877fbbecda65d122f5e1ba7222662926316308e
dtwelch <dtw.welch@gmail.com>
2019-03-01 12:23:08 -0500

    starting fresh, lets see if we can do syntax highlighting at least

--- src/edu/clemson/resolve/jetbrains/psi/ResBlock.java
+++
@@ -1,8 +0,0 @@
-package edu.clemson.resolve.jetbrains.psi;
-
-/**
- * A general (dummy) interface for generic scopeblock in RESOLVE. Thus, this applies to the top level scope for an
- * instance of {@link ResModuleDecl} and also {@link ResOperationLikeNode}s.
- */
-public interface ResBlock extends ResCompositeElement {
-}
